1a7cc29447ec0a42ea770dc3e3532fa6fffd0bee
spaceboy <chungisu@gmail.com>
2020-06-12 23:27:19 +0900

    80.2-get employee list from service.

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.luv2code.springboot.thymeleafdemo.entiry.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
@@ -22,6 +25,9 @@ public class EmployeeController {
 	// add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployee(Model theModel) {
+		
+		// get employees from db
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
