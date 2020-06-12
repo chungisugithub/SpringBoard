c160519989a3626c372ae732f80173ec7656707c
spaceboy <chungisu@gmail.com>
2020-06-12 16:43:35 +0900

    Add @Controller, @RequestMapping annotations and quick comment

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,5 +1,14 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
+	// load employee date
+	
+	// add mapping for "/list"
+	
 }
