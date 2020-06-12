7c2a541a0decc648e97b144a2c1e881f68dc630f
spaceboy <chungisu@gmail.com>
2020-06-12 16:47:22 +0900

    quick comment for loadData()

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.model.Employee;
 
 @Controller
 @RequestMapping("/employees")
@@ -9,6 +15,18 @@ public class EmployeeController {
 
 	// load employee date
 	
+	private List<Employee> employees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		// create employees
+		
+		// create the list
+		
+		// add to the list
+		
+	}
 	// add mapping for "/list"
 	
 }
