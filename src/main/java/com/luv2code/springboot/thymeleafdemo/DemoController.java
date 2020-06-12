413fd194871c3e8bd64a8852607aaa9b52fefcd9
spaceboy <chungisu@gmail.com>
2020-06-12 09:04:07 +0900

    78.2- add mapping "/hello"

--- src/main/java/com/luv2code/springboot/thymeleafdemo/DemoController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/DemoController.java
@@ -1,5 +1,17 @@
 package com.luv2code.springboot.thymeleafdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
 public class DemoController {
 
+	// create mapping for "/hello"
+	public String sayHello(Model theModel) {
+		
+		theModel.addAttribute("theDate", new java.util.Date());
+		
+		return "helloworld";
+		
+	}
 }
