b52c072c44065690796650b0ba6a808d82fe111f
spaceboy <chungisu@gmail.com>
2020-06-12 09:18:41 +0900

    add mapping annotation-@GetMapping("/hello")

--- src/main/java/com/luv2code/springboot/thymeleafdemo/DemoController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/DemoController.java
@@ -2,11 +2,13 @@ package com.luv2code.springboot.thymeleafdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
 	// create mapping for "/hello"
+	@GetMapping("/hello")
 	public String sayHello(Model theModel) {
 		
 		theModel.addAttribute("theDate", new java.util.Date());
