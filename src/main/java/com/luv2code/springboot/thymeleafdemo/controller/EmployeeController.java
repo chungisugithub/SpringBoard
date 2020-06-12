da2b720ff6d0af86a64d89012f74033e5cc6a66d
spaceboy <chungisu@gmail.com>
2020-06-12 16:54:18 +0900

    code for loadData()

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -15,16 +16,23 @@ public class EmployeeController {
 
 	// load employee date
 	
-	private List<Employee> employees;
+	private List<Employee> theEmployees;
 	
 	@PostConstruct
 	private void loadData() {
 		
 		// create employees
+		Employee empl = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
+		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@luv2code.com");
+		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@luv2code.com");
 		
 		// create the list
+		theEmployees = new ArrayList<>();
 		
 		// add to the list
+		theEmployees.add(empl);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
 		
 	}
 	// add mapping for "/list"
