e18e20a41e19cf327c2a58ec8dca02b99bdb5a78
spaceboy <chungisu@gmail.com>
2020-06-12 23:00:00 +0900

    80.2-remove the in-memory employee code

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -16,27 +16,6 @@ import com.luv2code.springboot.thymeleafdemo.entiry.Employee;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee date
-	
-	private List<Employee> theEmployees;
-	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employees
-		Employee empl = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@luv2code.com");
-		
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		// add to the list
-		theEmployees.add(empl);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-		
-	}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
