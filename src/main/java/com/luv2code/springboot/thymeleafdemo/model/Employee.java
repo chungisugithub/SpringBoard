1003ef310f9bfad044d3cf40fdddb97117b32164
spaceboy <chungisu@gmail.com>
2020-06-12 16:50:10 +0900

    update constructor add id parameter

--- src/main/java/com/luv2code/springboot/thymeleafdemo/model/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/Employee.java
@@ -11,7 +11,8 @@ public class Employee {
 		
 	}
 
-	public Employee(String firstName, String lastName, String email) {
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
